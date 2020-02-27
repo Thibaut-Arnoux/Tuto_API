ae3c4055373242b3d91a3a19391f81059cd88d6e
Thibaut-Arnoux <thib.arnoux@gmail.com>
2020-02-27 21:56:48 +0100

    authentification done

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/Thing');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -25,52 +26,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-
-    thing.save()
-        .then(() => res.status(201).json({
-            message : 'Objet enregistré !'
-        }))
-        .catch(error => res.status(400).json({
-            error
-        }));
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id : req.params.id}, {
-        _id : req.params.id,
-        title : req.body.title,
-        descriptions : req.body.descriptions,
-        price : req.body.price,
-        imageUrl : req.body.imageUrl,
-        userId : req.body.userId,
-    })
-    .then(() => res.status(200).json({ message : 'Objet modifié !'}))
-    .catch(error => res.status(400).json({error}));
-});
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({_id : req.params.id})
-    .then(thing => res.status(200).json({ message : 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id : req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({
-        error
-    }));
-});
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
---
+++ routes/stuff.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
+
+router.post('/', auth, stuffCtrl.createThing);
+
+router.put('/:id', auth, stuffCtrl.modifyThing);
+
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+
+router.get('/:id', auth, stuffCtrl.getOneThing);
+
+router.get('/', auth, stuffCtrl.getAllThing);
+
+module.exports = router;
